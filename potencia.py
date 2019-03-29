dbedd6bc3b5460c82dbfdb35a980abf47c47e42e
Estefany Rueda <yurley.rueda.2019@upb.edu.co>
2019-03-29 13:37:08 -0500

    Ejercicio1

--- potencia.py
+++ potencia.py
@@ -1,15 +1,13 @@
 
 def a_power_b(a,b):
 	prod=1
-	
+
 	for i in range(0,b):
 		prod=prod*a
-	
+
 	return prod
 
-while True:
-	a=int(input("Ingrese la base: "))
-	b=int(input("Ingrese la potencia: "))
-	print(a_power_b(a,b))
-	if a==0:
-		break
+a=int(input("Ingrese la base: "))
+b=int(input("Ingrese la potencia: "))
+
+print(a_power_b(a,b))
